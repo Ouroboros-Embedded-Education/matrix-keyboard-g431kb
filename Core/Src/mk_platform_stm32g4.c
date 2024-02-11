e19d18b55f2a66be56c044d363f93c0d6dff152b
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2024-02-10 21:15:25 -0300

    :minidisc: example finished, Lcd and Keypad working fine!

--- Core/Src/mk_platform_stm32g4.c
+++ Core/Src/mk_platform_stm32g4.c
@@ -1,21 +1,20 @@
+
 #include "mk_platform.h"
 
 /* Insert here the include of gpio driver of your chipset*/
 #include <stm32g4xx.h>
 
 void _mk_gpio_write(mk_gpio_t *gpio, mk_gpio_state_e val){
-    GPIO_TypeDef *GPIO;
-    uint32_t Pin;
+	GPIO_TypeDef *GPIO;
 
-    GPIO = (GPIO_TypeDef*)(gpio->GPIO);
-    Pin = (uint32_t)(gpio->pin);
+	GPIO = (GPIO_TypeDef*)(gpio->GPIO);
     if (val == MK_GPIO_LOW){
         // write low value on the gpio port
-        HAL_GPIO_WritePin(GPIO, Pin, GPIO_PIN_RESET);
+    	HAL_GPIO_WritePin(GPIO, gpio->pin, GPIO_PIN_RESET);
     }
     else{
         // write high value on the gpio port
-        HAL_GPIO_WritePin(GPIO, Pin, GPIO_PIN_SET);
+    	HAL_GPIO_WritePin(GPIO, gpio->pin, GPIO_PIN_SET);
     }
 }
 
@@ -23,15 +22,13 @@ mk_gpio_state_e _mk_gpio_read(mk_gpio_t *gpio){
     // check the gpio state and return the value:
     // MK_GPIO_LOW if the state is low
     // MK_GPIO_HIGH if the state was high
-    GPIO_TypeDef *GPIO;
-    uint32_t Pin;
+	GPIO_TypeDef *GPIO;
 
-    GPIO = (GPIO_TypeDef*)(gpio->GPIO);
-    Pin = gpio->pin;
-    if (HAL_GPIO_ReadPin(GPIO, Pin) == GPIO_PIN_RESET){
-        return MK_GPIO_LOW;
-    }
-    else{
-        return MK_GPIO_HIGH;
-    }
+	GPIO = (GPIO_TypeDef*)(gpio->GPIO);
+	if (HAL_GPIO_ReadPin(GPIO, gpio->pin) == GPIO_PIN_RESET){
+		return MK_GPIO_LOW;
+	}
+	else{
+		return MK_GPIO_HIGH;
+	}
 }
